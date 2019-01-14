c68bca78d72bc205b7bdcd4b0de024198adb310e
Joshua Barrass <josh.barrass.work@gmail.com>
2019-01-14 17:05:07 +0000

    All unit tests pass

--- y1Term2Assignment1.py
+++ y1Term2Assignment1.py
@@ -21,6 +21,68 @@ Author of template:
 import numpy as np
 
 # TODO: Assignment Task 1: write any utility functions you might find useful
+def month_probability(months):
+    """Calculates your probability of being born in any given month knowing
+    the number of days in each month.
+
+    Parameters
+    ----------
+    months : dictionary {name_of_month: days_in_the_month}
+
+    Returns
+    -------
+    probabilities : dictionary {name_of_month: probability}
+        This is the normalised probability of being born in the given month
+
+    """
+
+    total_days = sum(months.values())
+    probabilities = months.copy()
+    for key, val in months.items():
+        probabilities[key] = val/total_days
+
+    return probabilities
+
+def student_chance_of_matching(name, probabilities=None):
+    """Calculates your probability of the first letter of an individual student's
+    name matching the first letter of their birth month.
+
+    Parameters
+    ----------
+    name : name of student
+    months : dictionary {name_of_month: days_in_the_month}
+
+    Returns
+    -------
+    probability : float
+
+    """
+
+    probability = 0.0
+    for key, val in probabilities.items():
+        if key[0].lower() == name[0].lower():
+            probability += val
+
+    return probability
+
+def inverse_probabilities(probabilities):
+    """Inverts all probabilities in a probability dictionary as inverse = (1-p)
+
+    Parameters
+    ----------
+    probabilities : dictionary {key: probability}
+
+    Returns
+    -------
+    inv_probabilities: dictionary {key: inverse_probability}
+
+    """
+    inv_probabilities = {}
+    for key, val in probabilities.items():
+        inv_probabilities[key] = 1-val
+
+    return inv_probabilities
+
 # End of Task 1; proceed to Task 2
 
 def matching(months, names):
@@ -47,6 +109,17 @@ def matching(months, names):
 
     '''
     # TODO: Assignment Task 2: write function body
+    probabilities = month_probability(months)
+    student_matches = {}
+    for name in names:
+        student_matches[name] = student_chance_of_matching(name, probabilities=probabilities)
+    avg_matches = sum(student_matches.values())
+    inv_student_matches = inverse_probabilities(student_matches)
+    prob_no_match = np.product(list(inv_student_matches.values()))
+
+    return (avg_matches, prob_no_match)
+
+
     # End of Task 2; no further tasks
 
 if __name__ == '__main__':
